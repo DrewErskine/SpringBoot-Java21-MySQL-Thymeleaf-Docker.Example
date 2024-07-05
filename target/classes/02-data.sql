0421c0bb5d7521113f19c4fe089d31040982a017
Dmerskine19 <Dmerskine19@gmail.com>
2024-07-05 16:52:50 -0400

    docker..

---
+++ target/classes/02-data.sql
@@ -0,0 +1,5 @@
+USE peeppea_booksDB;
+
+INSERT INTO book (title, author) VALUES ('1984', 'George Orwell');
+INSERT INTO book (title, author) VALUES ('To Kill a Mockingbird', 'Harper Lee');
+INSERT INTO book (title, author) VALUES ('The Great Gatsby', 'F. Scott Fitzgerald');
