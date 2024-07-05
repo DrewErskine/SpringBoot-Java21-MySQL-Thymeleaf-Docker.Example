ec121790a53e49f005946de9124c27665cb00766
Dmerskine19 <Dmerskine19@gmail.com>
2024-07-04 21:07:08 -0400

    aws attempt day 2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO book (title, author) VALUES ('1984', 'George Orwell');
+INSERT INTO book (title, author) VALUES ('To Kill a Mockingbird', 'Harper Lee');
+INSERT INTO book (title, author) VALUES ('The Great Gatsby', 'F. Scott Fitzgerald');
